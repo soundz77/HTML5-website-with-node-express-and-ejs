ff2563d5152dbea77488536f8d28b4c86c221c35
soundz77 <simon_ap@gmx.net>
2024-07-01 20:51:49 +0200

    Removed routes

--- base-template/src/app.js
+++ base-template/src/app.js
@@ -1,12 +1,10 @@
 import express from "express";
 
 import configureMiddleware from "./config/configureMiddleware.js";
-import configureRoutes from "./config/configureRoutes.js";
 
 const app = express();
 
 // configuration
 configureMiddleware(app);
-configureRoutes(app);
 
 export default app;
--- base-template/src/controllers/index.js
+++
@@ -1,6 +0,0 @@
-const index = (req, res) => {
-  const title = "Basic Express App";
-  res.status(200).render("index", { title });
-};
-
-export default index;
