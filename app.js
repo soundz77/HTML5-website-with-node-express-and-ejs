624f1daf0fd19839160a670e1bc26c06ffb368ab
soundz77 <simon_ap@gmx.net>
2024-07-13 14:44:41 +0200

    Removed redundancy

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // Import dependencies
 import handleAsync from "express-async-handler";
 import setupApp from "./base-template/src/app.js";
-import configureMongoose from "./src/config/configureMongoose.js";
 import setupServer from "./base-template/src/server.js";
 import configureAppMiddleware from "./src/config/configureAppMiddleware.js";
 import configureRoutes from "./src/config/configureRoutes.js";
@@ -11,7 +10,6 @@ import globalErrorHandler from "./base-template/src/utils/errors/globalErrorHand
 const main = handleAsync(async () => {
   const app = setupApp(); // Initialize the app
 
-  await configureMongoose(); // Await database connection
   await setupServer(app); // Start the server
   await configureAppMiddleware(app); // Configure middleware
   configureRoutes(app); // Configure routes
--- base-template/src/config/configureMiddleware.js
+++ base-template/src/config/configureMiddleware.js
@@ -9,14 +9,14 @@ import { fileURLToPath } from "url";
 import httpLogger from "../utils/logging/HTTPlogger.js";
 import { strictCors } from "./middleware/corsConfig.js";
 import rateLimitConfig from "./middleware/rateLimitConfig.js";
-import helmetConfig from "./middleware/helmetConfig.js"; // Ensure this is configured properly
+import helmetConfig from "./middleware/helmetConfig.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const configureMiddleware = (app) => {
   app.use(httpLogger);
-  app.use(helmet(helmetConfig));
+  app.use(helmet(helmetConfig)); // Currently using defaults
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(express.json());
   app.use(RateLimit(rateLimitConfig));
